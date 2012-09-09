7b5a233d273ef5d790bc83249d5b6f7a762da41e
Karen Etheridge <ether@cpan.org>
2012-09-08 18:15:31 -0700

    prep for 1.004002

--- lib/strictures.pm
+++ lib/strictures.pm
@@ -5,7 +5,7 @@ use warnings FATAL => 'all';
 
 use constant _PERL_LT_5_8_4 => ($] < 5.008004) ? 1 : 0;
 
-our $VERSION = '1.004001'; # 1.4.1
+our $VERSION = '1.004002'; # 1.4.2
 
 sub VERSION {
   for ($_[1]) {
